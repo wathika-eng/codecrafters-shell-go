f9857e6d43c8e5eeb90386b63154994648d7bbd9
wathika-eng <wathika02@gmail.com>
2024-12-03 20:35:39 +0300

    refactor main function to improve user input handling and add exit command

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -11,15 +11,25 @@ import (
 var _ = fmt.Fprint
 
 func main() {
-	// Uncomment this block to pass the first stage
-
-	// Wait for user input
 	for {
-		fmt.Fprint(os.Stdout, "$ ")
-		reader, err := bufio.NewReader(os.Stdin).ReadString('\n')
+		reader, err := userInput()
 		if err != nil {
 			log.Fatal(err)
 		}
-		fmt.Printf(reader[:len(reader)-1] + ": command not found\n")
+
+		// Remove the newline character
+		command := reader[:len(reader)-1]
+
+		if command == "exit" {
+			fmt.Println("Exiting...")
+			break
+		}
+		fmt.Printf("%s: command not found\n", command)
 	}
+}
+
+func userInput() (string, error) {
+	fmt.Fprint(os.Stdout, "$ ")
+	reader, err := bufio.NewReader(os.Stdin).ReadString('\n')
+	return reader, err
 }
